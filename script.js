58812c7c08032987cee43c71513238245f8c3780
Dhruv Patel <122099280+dhruvurgr8@users.noreply.github.com>
2024-03-26 16:42:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+const openModal = document.getElementById("openModal");
+const modal = document.querySelector(".modal")
+const closeModal = document.querySelector(".close-modal");
+openModal.addEventListener('click',()=>{
+	modal.style.display = "block";
+})
+closeModal.addEventListener('click',()=>{
+	modal.style.display = "none";
+})
